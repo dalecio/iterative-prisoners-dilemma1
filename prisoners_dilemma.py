d0f8b7f987565460dfa31fe8cd1083630fbcb5f2
dalecio <dalecioman@yahoo.com>
2015-12-04 10:31:02 -0600

    Update prisoners_dilemma.py
    trying to figure this out, added 3rd player remark name dalecio.

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -172,7 +172,7 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
 
     ######
     ######
-    #
+    #dalecio
     elif player == 3:
         if getting_team_name:
             return 'loyal vengeful'
